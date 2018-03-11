26193154e6c121f5385ba53742dc8556d1341cd3
Ralf Yang <goody80762@gmail.com>
2018-03-11 22:09:05 +0900

    Create go_shell_install.sh

---
+++ go_shell_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+System=$(uname -s)-$(uname -m)
+        case $System in
+                Linux-x86_64)
+                        Sys_tag="linux-amd64.tar.gz";;
+        esac
+
+go_version="1.10"
+tar_ball="go$go_version.$Sys_tag"
+
+## Go install
+curl -L https://dl.google.com/go/$tar_ball -o ./$tar_ball
+sudo tar -C /usr/local -xzf $tar_ball
+export PATH=$PATH:/usr/local/go/bin
+go get github.com/erning/gorun
+sudo mv ~/go/bin/gorun /usr/local/bin/
+echo ':golang:E::go::/usr/local/bin/gorun:OC' | sudo tee /proc/sys/fs/binfmt_misc/register
