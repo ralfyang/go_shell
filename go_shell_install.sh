0036a96794e16bf3717a6a46c9e7461afa77474d
Ralf Yang <goody80762@gmail.com>
2018-03-13 11:15:47 +0900

    Update go_shell_install.sh

--- go_shell_install.sh
+++ go_shell_install.sh
@@ -11,7 +11,10 @@ tar_ball="go$go_version.$Sys_tag"
 ## Go install
 curl -L https://dl.google.com/go/$tar_ball -o ./$tar_ball
 sudo tar -C /usr/local -xzf $tar_ball
-export PATH=$PATH:/usr/local/go/bin
+export PATH="$PATH:/usr/local/go/bin"
+echo 'export PATH=$PATH:/usr/local/go/bin' > /etc/profile.d/go_path.sh
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bash_profile
 go get github.com/erning/gorun
 sudo mv ~/go/bin/gorun /usr/local/bin/
 echo ':golang:E::go::/usr/local/bin/gorun:OC' | sudo tee /proc/sys/fs/binfmt_misc/register
+bash -i
